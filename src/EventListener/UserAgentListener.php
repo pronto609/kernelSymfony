d0e458fe8184643a927b305f3237314c406116c2
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-08 08:17:52 +0300

    login routers

---
+++ src/EventListener/UserAgentListener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\EventListener;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\Mailer\Event\MessageEvent;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+
+class UserAgentListener implements EventSubscriberInterface
+{
+    public function __construct(
+        private readonly LoggerInterface $logger
+    ) {
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+//            'kernel.request' => 'onKernelRequest'
+            RequestEvent::class => 'onKernelRequest'
+        ];
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+
+        $userAgent = $request->headers->get('User-Agent');
+        $this->logger->info(
+            sprintf('The User-Agent is %s', $userAgent)
+        );
+
+    }
+
+}
