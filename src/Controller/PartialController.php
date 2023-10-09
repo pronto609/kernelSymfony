179e73772d46711272075f8c2a9caaeeed39551f
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-09 17:17:16 +0300

    use subrequest

---
+++ src/Controller/PartialController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PartialController extends AbstractController
+{
+    public function trendingQuotes(): Response
+    {
+        $quotes = $this->getTrendingQuotes();
+
+        return $this->render('partial/trendingQuotes.html.twig', [
+            'quotes' => $quotes
+        ]);
+    }
+
+    private function getTrendingQuotes()
+    {
+        return [
+            [
+                'author' => 'Wernher von Braun, Rocket Engineer',
+                'link' => 'https://en.wikipedia.org/wiki/Wernher_von_Braun',
+                'quote' => 'Our two greatest problems are gravity and paperwork. We can lick gravity, but sometimes the paperwork is overwhelming.',
+            ],
+            [
+                'author' => 'Aaron Cohen, NASA Administrator',
+                'link' => 'https://en.wikipedia.org/wiki/Aaron_Cohen_(Deputy_NASA_administrator)',
+                'quote' => 'Let\'s face it, space is a risky business. I always considered every launch a barely controlled explosion.',
+            ],
+            [
+                'author' => 'Christa McAuliffe, Challenger Astronaut',
+                'link' => 'https://en.wikipedia.org/wiki/Christa_McAuliffe',
+                'quote' => 'If offered a seat on a rocket ship, don\'t ask what seat. Just get on.',
+            ],
+        ];
+    }
+}
